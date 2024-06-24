3845222b622287b831789b329211d3c601f94c7f
joshuawaheed <joshua.waheed@gmail.com>
2024-06-24 12:21:01 +0100

    Section 24: Plugin Development: Blocks, Gutenberg & React

---
+++ fictional-university/app/public/wp-content/plugins/are-you-paying-attention/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '12d7a3e17ca7d87d29d6');
---
+++ fictional-university/app/public/wp-content/plugins/are-you-paying-attention/src/index.js
@@ -0,0 +1,42 @@
+wp.blocks.registerBlockType("ourplugin/are-you-paying-attention", {
+    attributes: {
+        skyColour: { type: "string" },
+        grassColour: { type: "string" }
+    },
+    category: "common",
+    icon: "smiley",
+    title: "Are You Paying Attention?",
+    edit: function(props) {
+        function updateGrassColour(event) {
+            props.setAttributes({ grassColour: event.target.value });
+        }
+        function updateSkyColour(event) {
+            props.setAttributes({ skyColour: event.target.value });
+        }
+        return wp.element.createElement("div", null, [
+            wp.element.createElement(
+                "input",
+                {
+                    type: "text",
+                    placeholder: "sky colour",
+                    value: props.attributes.skyColour,
+                    onChange: updateSkyColour
+                },
+                null
+            ),
+            wp.element.createElement(
+                "input",
+                {
+                    type: "text",
+                    placeholder: "grass colour",
+                    value: props.attributes.grassColour,
+                    onChange: updateGrassColour
+                },
+                null
+            )
+        ]);
+    },
+    save: function(props) {
+        return null;
+    }
+});
